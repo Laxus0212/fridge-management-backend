645e1f7c76a662dadef47d313537ff794199315a
Zsolt Váradi <varadi.zsolt@novaservices.hu>
2025-01-26 19:07:12 +0100

    add sequelize, model, providers to module and configure cors exception for frontend url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -28,9 +28,74 @@ import { ShoppingListsModule } from './shopping-lists/shopping-lists.module';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { User } from './users/models/user.model';
+import { Family } from './families/models/family.model';
+import { Product } from './products/models/product.model';
+import { Shelf } from './shelves/models/shelf.model';
+import { Fridge } from './fridges/models/fridge.model';
+import { Chat } from './messages/models/chat.model';
+import { Recipe } from './recipes/models/recipe.model';
+import { Ingredient } from './recipes/models/ingredient.model';
+import { Message } from './messages/models/message.model';
+import { ShoppingList } from './shopping-lists/models/shopping-list.model';
+import { ShoppingListItem } from './shopping-lists/models/shopping-list-item.model';
+import { JwtModule } from '@nestjs/jwt';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Invitation } from './families/models/invitation.model';
 
 @Module({
-  imports: [AuthModule, FamiliesModule, FridgesModule, MessagesModule, ProductsModule, RecipesModule, ShelvesModule, ShoppingListsModule, UsersModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'varadi',
+      password: 'password',
+      database: 'fridge_database',
+      models: [
+        Family,
+        User,
+        Product,
+        Shelf,
+        Fridge,
+        Message,
+        Chat,
+        Recipe,
+        Ingredient,
+        ShoppingList,
+        ShoppingListItem,
+      ],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([
+      User,
+      Family,
+      Invitation,
+      Product,
+      Shelf,
+      Fridge,
+      Chat,
+      Recipe,
+      Ingredient,
+      Message,
+      ShoppingList,
+      ShoppingListItem,
+    ]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'your-secret-key',
+      signOptions: { expiresIn: '1h' },
+    }),
+    UsersModule,
+    AuthModule,
+    FamiliesModule,
+    FridgesModule,
+    MessagesModule,
+    ProductsModule,
+    RecipesModule,
+    ShelvesModule,
+    ShoppingListsModule,
+  ],
   controllers: [
     AppController,
     AuthController,
@@ -43,6 +108,30 @@ import { UsersModule } from './users/users.module';
     ShoppingListsController,
     UsersController,
   ],
-  providers: [AppService, AuthService, FamiliesService, FridgesService, MessagesService, ProductsService, RecipesService, ShelvesService, ShoppingListsService, UsersService],
+  providers: [
+    AppService,
+    AuthService,
+    FamiliesService,
+    FridgesService,
+    MessagesService,
+    ProductsService,
+    RecipesService,
+    ShelvesService,
+    ShoppingListsService,
+    UsersService,
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // middleware
+  }
+
+  // CORS engedélyezése
+  static configure(app) {
+    app.enableCors({
+      origin: 'http://localhost:8100', // Engedélyezett url (frontend)
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true, // Sütiknek
+    });
+  }
+}
