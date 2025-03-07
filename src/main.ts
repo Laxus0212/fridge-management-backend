533a5f4bf774e17e7ea9e88b41dde01a03908652
Zsolt Váradi <varadi.zsolt@novaservices.hu>
2025-03-07 09:59:14 +0100

    update main ts with cookie parser and prevent cross origin error by setting header options

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
   AppModule.configure(app);
+
+  app.use((req, res, next) => {
+    res.header('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+    res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+    next();
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
