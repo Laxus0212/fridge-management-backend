12e4b656056d1bf898e96f3b0a02f5a787a6ae22
Zsolt Váradi <varadi.zsolt@novaservices.hu>
2025-03-09 20:48:55 +0100

    update SQL grants to include password identification for varadi and root users

--- src/database/sql-scripts/init.sql
+++ src/database/sql-scripts/init.sql
@@ -1,7 +1,5 @@
--- Grant external access to varadi user
-GRANT ALL PRIVILEGES ON fridge_database.* TO 'varadi'@'%';
+GRANT ALL PRIVILEGES ON fridge_database.* TO 'varadi'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 FLUSH PRIVILEGES;
 
--- Grant external access to root user
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
