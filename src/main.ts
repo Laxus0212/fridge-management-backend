69c0fda87bf424c7111d6013e13292dd3806ed8e
Zsolt Váradi <varadi.zsolt@novaservices.hu>
2025-03-22 19:53:10 +0100

    update main.ts to integrate WebSocket support using IoAdapter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+
+  app.useWebSocketAdapter(new IoAdapter());
 
   AppModule.configure(app);
 
